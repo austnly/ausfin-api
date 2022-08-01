a4386208b9493fa34d3da599f8bf115bb2805d4c
Austin Ly <austinly@icloud.com>
2022-08-01 12:42:17 +1000

    Added CrossOrigin permission for localhost

--- src/main/java/ausfin/api/CalculatorController.java
+++ src/main/java/ausfin/api/CalculatorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3001")
 @RestController
 @RequestMapping
 public class CalculatorController {
